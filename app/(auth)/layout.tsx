e27f44ea3a71b658f4350f9862271fc996d8f418
singh-kanwarpreet <kanwarpreet388@gmail.com>
2026-05-16 15:28:12 +0530

    Add authentication routes and middleware for Clerk integration

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+export default function authLayout({
+  children,
+}: { children : React.ReactNode }){
+    return (<div className="flex items-center justify-center min-h-screen">{children}</div>)
+}
